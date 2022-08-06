047ea012567378b55e529dfd200dbff3915c6781
Pedro-AugusCoelho <peaugusto.coelho@gmail.com>
2022-08-05 22:53:19 -0300

    commit

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,17 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="bg-primary flex row">
+    <q-card class="col-3 bg-secondary">
+      <q-card-section class="flex items-center">
+        <q-avatar color="primary" text-color="white" size="80px">PA</q-avatar>
+        <q-card-section class="flex column">
+          <span class="text-h6">Pedro Augusto</span>
+          <span class="color-gray">@Coelho#2127</span>
+        </q-card-section>
+     </q-card-section>
+    </q-card>
+
+    <q-card class="col-5 bg-accent"></q-card>
+    <q-card class="col-4 bg-positive"></q-card>
   </q-page>
 </template>
 
